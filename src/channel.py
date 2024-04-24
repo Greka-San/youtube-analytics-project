4b1ec6f99aeda6127e830362521e3e07ff5ef517
GrekaSan <Greka-San@yandex.ru>
2024-04-24 23:18:44 +0300

    home work 2

--- src/channel.py
+++ src/channel.py
@@ -6,13 +6,31 @@ import json
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = "https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A"
+        self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
+
+    @classmethod
+    def get_service(cls):
+        """возвращающий объект для работы с YouTube API"""
+        return cls.youtube
+
+    def to_json(self, file_name):
+        """сохраняющий в файл значения атрибутов экземпляра"""
+        with open(file_name, 'w', encoding='utf8') as file:
+            json.dump(self.channel, file)
+
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
