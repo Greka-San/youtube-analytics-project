2dc6a014a59f90cdab0887e96f992022f616971a
GrekaSan <Greka-San@yandex.ru>
2024-05-21 22:19:43 +0300

    home work 6

--- src/video.py
+++ src/video.py
@@ -9,14 +9,21 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 class Video:
     def __init__(self, video_id):
-        self.video_id = video_id
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = 'https://www.youtube.com/watch?v=' + self.video_id
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.title = video_response['items'][0]['snippet']['title']
+            self.url = 'https://www.youtube.com/watch?v=' + self.video_id
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
